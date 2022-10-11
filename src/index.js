f48fb871a277bd7a1cf11f83f345d255f0f34af2
dulhafi <dulhafi19@gmail.com>
2022-10-11 09:36:33 +0700

    add

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Book from './pages/Book';
+import './css/App.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const AppRouter = () => (
+  <BrowserRouter>
+    <Route exact path="/katalog-buku" component={Home} />
+    <Route path="/katalog-buku/book/:id" component={Book} />
+  </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<AppRouter />, document.getElementById('app'));
+
+if (process.env.NODE_ENV === 'development') {
+  module.hot.accept();
+}
